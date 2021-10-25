cc219bb18c1cb3fbc8141f2c194d24cdcb11c06c
leschtsch <alehazv@mail.ru>
2021-10-25 12:53:41 +0300

    testing1

--- main.py
+++ main.py
@@ -1,58 +1,83 @@
+"""
+Данный модуль импортирует генератор и реализует основной цикл программы.
+"""
+
 import sys
 import os.path
 import time
 
 import setup
-from easygui import fileopenbox, filesavebox
+from easygui import fileopenbox, filesavebox  # чтобы открыть генератор и сохранить задачи
 
 from export import export
 from interface import Interface
 
 
 def generator_load():
+    """
+    Данная функция получает путь к файлу с генератором,
+    используя easygui, после чего импортирует его и возвращает экземпляр класса генератора.
+    :return: объект генератора.
+    """
     generator = fileopenbox(msg='Выберите генератор', default='Генераторы/generator.py',
-                            filetypes=['*.py'])
+                            filetypes=['*.py'])  # получение поти к генератору
     if not generator:
-        return None
-    generator = list(os.path.split(generator))
-    sys.path.append(generator[0])
-    sys.path.append(os.path.join(generator[0], 'subgenerators'))
+        return
+
+    generator = list(os.path.split(generator))  # получение пути и имени модуля из пути к файлу
     generator[1] = generator[1].strip('.py')
-    generator = __import__(generator[1])
+
+    sys.path.append(generator[0])  # вставка пути чтобы сработал импорт
+    sys.path.append(os.path.join(generator[0], 'subgenerators'))
+
+    generator = __import__(generator[1])  # импорт
     return generator.Generator()
 
 
-generator = generator_load()
-if generator:
-    interface = Interface(generator.window_size, generator.needed_params, generator.status)
+def mainloop(generator):
+    """
+    Данная функция реализует основной цикл программы.
+    :param generator: объект - генератор
+    :return: None
+    """
+    if generator is None:
+        return
 
+    # создание интерфейса и задание параметров по умолчанию
+    interface = Interface(generator.window_size, generator.needed_params, generator.status)
     for i in generator.needed_params:
         if i.name in generator.selected_params and (i.type_ == 'text' or i.type_ == 'number'):
             interface.widgets[i.name][1].set_text(generator.selected_params[i.name])
+    # создание интерфейса и задание параметров по умолчанию
 
     running = True
-
     while running:
-        time.sleep(0.01)
+        time.sleep(0.01)  # иначе курсор ввода текста мерцает
+
+        # установка в интерфейс статусов генератора
         interface.status = generator.status
         interface.ready = generator.ready
+        # установка в интерфейс статусов генератора
 
-        params = interface.tick()
+        params = interface.tick()  # получение параметров генератора от интерфейса
         if isinstance(params, dict):
             generator.set_params(params)
         elif params == 'Generate':
             interface.status = 'генерация задач'
             interface.tick()
-            generated = generator.generate()
+            generated = generator.generate()  # [tasks, answers, default name]
             tasks = generated[0]
             answers = generated[1]
             interface.status = 'сохранение задач'
             interface.tick()
             save_path = filesavebox(msg='сохранение файла', default=generated[2])
-            if save_path:
+            if save_path is not None:
                 export(tasks, answers, save_path)
                 interface.generated += 1
             interface.status = generator.status
             interface.tick()
         else:
             break
+
+
+mainloop(generator_load())
--- setup.py
+++ setup.py
@@ -1,9 +1,50 @@
+"""
+Данный модуль нужен, чтобы установить нужные модули, если их нет.
+"""
+
 import sys
 import subprocess
 import pkg_resources
 
-required = {'pygame', 'pygame-gui', 'python-docx', 'lxml', 'easygui', 'latex2mathml'}
-installed = {pkg.key for pkg in pkg_resources.working_set}
-missing = required - installed
-if missing:
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', *missing])
+
+def install_missing():
+    """
+    Данная функция устанавливает необходимые модули
+    :return: None
+    """
+    # получение списка недостающих модулей
+    required = {'pygame', 'pygame-gui', 'python-docx', 'lxml', 'easygui', 'latex2mathml'}
+    installed = {pkg.key for pkg in pkg_resources.working_set}
+    missing = required - installed
+    # получение списка недостающих модулей
+
+    if not missing:  # проверка, надо ли что-то устанавливать
+        return
+
+    '''
+    установка модулей
+    Я написал четыре варианта, потому что не знаю,
+    что из этого на каком компьютере сработает.
+    В коде со StackOverFlow был check_call, но он устарел
+    '''
+    try:
+        subprocess.run([sys.executable, '-m', 'pip', 'install', *missing], check=True)
+        return
+    except subprocess.CalledProcessError:
+        pass
+
+    try:
+        subprocess.run(['pip', 'install', *missing], check=True)
+        return
+    except subprocess.CalledProcessError:
+        pass
+
+    try:
+        subprocess.run(['pip3', 'install', *missing], check=True)
+        return
+    except subprocess.CalledProcessError:
+        pass
+    # установка модулей
+
+
+install_missing()
