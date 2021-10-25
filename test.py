82bbfe25734c27dd0126c429da13d3df165a0912
leschtsch <72019469+leschtsch@users.noreply.github.com>
2021-10-25 21:52:38 +0300

    v1.0

--- test.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import sys
-import pkg_resources
-
-required = {'pygame', 'pygame-gui', 'python-docx', 'lxml', 'easygui', 'latex2mathml'}
-installed = {pkg.key for pkg in pkg_resources.working_set}
-missing = required - installed
-if missing:
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', *missing])
