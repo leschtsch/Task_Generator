2523239c7764f2c64212b4ee327f82e8c4195265
leschtsch <alehazv@mail.ru>
2022-03-05 14:57:42 +0300

    AltTex formulas

--- Генераторы/Тест/generator_3.py
+++ Генераторы/Тест/generator_3.py
@@ -31,7 +31,7 @@ class Generator:
                   LatexFormula('x^2=0', space=1),
                   Picture(getcwd() + '\\Генераторы\\Тест\\subgenerators\\coh.png', width=5)], style='number'),
             Picture(getcwd() + '\\Генераторы\\Тест\\subgenerators\\coh.png', width=10),
-        AlttexFormula('sqrt(t(abc))')]
+        AlttexFormula('A3A(c(t(   y = ))(sqrt(t(x))))(c(t(   y = x))(i(t(2))()))(c(t(   y = ))(|(t(x))(t(x+1))))')]
         aels = [
             Sentence([Text('тест '),
                       Picture(getcwd() + '\\Генераторы\\Тест\\subgenerators\\coh.png', width=1, inline=True),
